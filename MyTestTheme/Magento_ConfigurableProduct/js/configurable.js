ce70fc5999d63cdd1249bed8c7141846c7536013
Abdul Matin <matinict@gmail.com>
2020-02-12 14:04:03 +0600

    Update configurable.js

--- MyTestTheme/Magento_ConfigurableProduct/js/configurable.js
+++ MyTestTheme/Magento_ConfigurableProduct/js/configurable.js
@@ -1,2 +1,12 @@
- require(["Magento_ConfigurableProduct/js/configurable"], function(Configurable){
- });
+define([
+    'jquery',
+    'jquery/ui',
+    'Magento_ConfigurableProduct/js/configurable'
+], function($){
+
+    $.widget('silvan.configurable', $.mage.configurable, {
+        //code you want to override
+    });
+
+    return $.silvan.configurable;
+});
